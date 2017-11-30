08a04e234f3bf4311e7229e19d679072f11062f4
TAYA <waiukigroup@gmail.com>
2017-11-30 23:20:32 +0800

    Add Model PinnedLocation

--- pinnedpjt/pinnedapp/admin.py
+++ pinnedpjt/pinnedapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from pinnedapp.models import PinnedLocation
+
+admin.site.register(PinnedLocation)
--- pinnedpjt/pinnedapp/models.py
+++ pinnedpjt/pinnedapp/models.py
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+
+class PinnedLocation(models.Model):
+    """場所"""
+    name = models.CharField('場所名', max_length=255)
+    lat = models.FloatField('緯度')
+    lng = models.FloatField('経度')
+
+    def __str__(self):
+        return self.name
