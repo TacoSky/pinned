0cc937ec22bbb91e67d69040c0f09be48a2f38bc
TAYA <waiukigroup@gmail.com>
2017-12-08 11:26:46 +0800

    Ajust size

--- pinnedpjt/static/js/pinned.js
+++ pinnedpjt/static/js/pinned.js
@@ -1,21 +1,43 @@
-(function() {
-    'use strict';
 
-    class MapView {
-        constructor(id) {
-            this.id = id;
-        }
-        initMap() {
-            this.map = new google.maps.Map(document.getElementById(this.id), {
-                center: {lat: -34.397, lng: 150.644},
-                zoom: 8
-            });
-        }
+'use strict';
+
+/*
+ * global object
+ */
+var ctrlMap;
+
+/*
+ * class
+ */
+class MapView {
+    constructor(id) {
+        this.divid = id;
+        this.map = new google.maps.Map(document.getElementById(id), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+        this.adjust();
+    }
+    adjust(){
+        var hwin = $(window).height();
+        var hnav = $('.nav-wrapper').height();
+        $("#"+this.divid).css('height', (hwin - hnav - 2) + "px"); //可変部分の高さを適用
     }
 
-    var ctrlMap = new MapView('map');
-    console.log("test1");
-})();
 
-    console.log("test2");
+}
+
+/*
+ * call back
+ */
+function cbInitMap(){
+    ctrlMap = new MapView('map');
+}
+
+/*
+ * call method
+ */
+$(window).on('resize', function(){
+      ctrlMap.adjust();
+})
 
